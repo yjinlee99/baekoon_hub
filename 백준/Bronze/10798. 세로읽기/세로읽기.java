281f47f73d835320f9998a9b8809f6c1632f0d6b
yjinlee99 <73848116+yjinlee99@users.noreply.github.com>
2023-11-29 16:08:18 +0900

    [Bronze I] Title: 세로읽기, Time: 204 ms, Memory: 17608 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,21 +1,23 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        char[][] arr1 = new char[5][15];
+        String str = "";
 
-        String[] s = new String[5];
-        int max = 0;
-        for(int i=0; i<5; i++) {
-            s[i] = sc.next();
-            if(s[i].length() >= max) max = s[i].length();
+        for (int i = 0; i < arr1.length; i++) {
+            str = sc.next();
+            for (int j = 0; j < str.length(); j++) {
+                arr1[i][j] = str.charAt(j);
+            }
         }
-
-        for(int i=0; i<max; i++){
-            for(int j=0; j<5; j++) {
-                if(i < s[j].length()) {
-                    System.out.print(s[j].charAt(i));
-                }
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(arr1[j][i] != '\0'){
+                    System.out.print(arr1[j][i]);}
             }
         }
     }
